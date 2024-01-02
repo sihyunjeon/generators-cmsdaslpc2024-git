61ac243654ab71ae7dbe36f36f2a89bb94d1cc37
Si Hyun Jeon <sjeon@cmslpc179.fnal.gov>
2024-01-02 15:25:41 -0600

    update scripts

--- fragment/zto2e_012j.py
+++ fragment/zto2e_012j.py
@@ -4,7 +4,7 @@ import os
 GENGRIDPACKPATH = os.getenv("GENGRIDPACKPATH")
 
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
-    args = cms.vstring(GENGRIDPACKPATH + "/bin/MadGraph5_aMCatNLO/dytoee-0j_slc7_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz"),
+    args = cms.vstring(GENGRIDPACKPATH + "/bin/MadGraph5_aMCatNLO/ztoee-012j_slc7_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz"),
     nEvents = cms.untracked.uint32(5000),
     numberOfParameters = cms.uint32(1),
     outputFile = cms.string('cmsgrid_final.lhe'),
@@ -19,9 +19,24 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
     PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
         pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+            'JetMatching:setMad = off',
+            'JetMatching:scheme = 1',
+            'JetMatching:merge = on',
+            'JetMatching:jetAlgorithm = 2',
+            'JetMatching:etaJetMax = 5.',
+            'JetMatching:coneRadius = 1.',
+            'JetMatching:slowJetPower = 1',
+            'JetMatching:doShowerKt = off', 
+            'JetMatching:qCut = 19.',
+            'JetMatching:nQmatch = 4',
+            'JetMatching:nJetMax = 2',
+            'TimeShower:mMaxGamma = 4.0'
+        ),
         parameterSets = cms.vstring(
             'pythia8CommonSettings',
             'pythia8CP5Settings',
+            'processParameters',
         )
     ),
     comEnergy = cms.double(13600),
